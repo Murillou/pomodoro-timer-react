9a1fae3e754c0f1818a1ec116679554edf086895
Murillou <Murillovinicius13@hotmail.com>
2024-05-03 04:00:50 -0300

    [refactor] estilizando inputs do home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,13 +16,27 @@ export function Home() {
         <form action="">
           <FormContainer>
             <label htmlFor="task">Vou trabalhar em</label>
-            <TaskInput placeholder="Dê um nome para o seu projeto" id="task" />
+            <TaskInput
+              list="task-suggestions"
+              placeholder="Dê um nome para o seu projeto"
+              id="task"
+            />
+
+            <datalist id="task-suggestions">
+              <option value="Projeto 1"></option>
+              <option value="Projeto 2"></option>
+              <option value="Projeto 3"></option>
+              <option value="Projeto 4"></option>
+            </datalist>
 
             <label htmlFor="minutesAmount  ">durante</label>
             <MinutesAmountInput
               type="number"
               id="minutesAmount"
               placeholder="00"
+              step={5}
+              min={5}
+              max={60}
             />
 
             <span>minutos.</span>
