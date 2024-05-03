1fc9f9e7b1e2642bc7f5181476a80276adc74c4b
Murillou <Murillovinicius13@hotmail.com>
2024-05-02 22:22:22 -0300

    [NEW Configurando rotas]

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button/Button';
 import { defaultTheme } from './styles/themes/default';
+import { BrowserRouter } from 'react-router-dom';
+
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-      <Button />
     </ThemeProvider>
   );
 }
